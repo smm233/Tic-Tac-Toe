f5d15311c5e86f79638dcb10e0b9828eef6a3758
shil-mil <shyshy122303@gmail.com>
2025-03-08 20:10:48 +0800

    Select tile and place X or O

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var board;
 var playerX = "X";
 var playerO = "O";
-var firstPlayer = "X";
+var currentPlayer = playerO;
 var gameOver = false;
 
 window.onload = function() {
@@ -26,7 +26,34 @@ function startGame() {
             if(b == 0 || b == 1) {
                 tile.classList.add("vertical-line");
             }
+            tile.addEventListener("click", selectTile);
             document.getElementById("board").append(tile);
         }
     }
+}
+
+function selectTile() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-");
+    let a = parseInt(coords[0]);
+    let b = parseInt(coords[1]);
+
+    if(board[a][b] == ' ') {
+        board[a][b] = currentPlayer;
+    } else {
+        return;
+    }
+    
+    this.innerText = currentPlayer;
+
+    if (currentPlayer == playerO) {
+        currentPlayer = playerX;
+    } else {
+        currentPlayer = playerO;
+    }
+
+    checkGame();
 }
