55d05d42168c046c0fc37454492811b8c00da95f
shil-mil <shyshy122303@gmail.com>
2025-03-08 20:53:48 +0800

    Check if there's a winner

--- script.js
+++ script.js
@@ -30,6 +30,10 @@ function startGame() {
             document.getElementById("board").append(tile);
         }
     }
+
+    let winner = document.getElementById("winner-player")
+        winner.classList.add("winner-player");
+        winner.append("Player " + value + " wins!");
 }
 
 function selectTile() {
@@ -55,5 +59,61 @@ function selectTile() {
         currentPlayer = playerO;
     }
 
+    displayPlayer(currentPlayer);
     checkGame();
+}
+
+function checkGame() {
+    //horizontal checking
+    for(let a = 0; a < 3; a++) {
+        if(board[a][0] == board[a][1] && board[a][1] == board[a][2] && board[a][0] != ' ') {
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(a.toString() + "-" + i.toString());
+                tile.classList.add("winner-tiles");
+            }
+            gameOver = true;
+            displayPlayer(board[a][0]);
+            return;
+        }
+    }
+
+    //vertical checking
+    for(let b = 0; b < 3; b++) {
+        if(board[0][b] == board[1][b] && board[1][b] == board[2][b] && board[0][b] != ' ') {
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(i.toString() + "-" + b.toString());
+                tile.classList.add("winner-tiles");
+            }
+            gameOver = true;
+            displayPlayer(board[0][b].toString());
+            return;
+        }
+    }
+
+    //left diagonal checking
+    if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        for (let i = 0; i < 3; i++) {
+            let tile = document.getElementById(i.toString() + "-" + i.toString());
+            tile.classList.add("winner-tiles");
+        }
+        gameOver = true;
+        displayPlayer(board[0][0].toString());
+        return;
+    }
+
+    //right diagonal checking
+    if(board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != ' ') {
+        let tile = document.getElementById("2-0");
+        tile.classList.add("winner-tiles");
+
+        tile = document.getElementById("1-1");
+        tile.classList.add("winner-tiles");
+
+        tile = document.getElementById("0-2");
+        tile.classList.add("winner-tiles");
+
+        gameOver = true;
+        displayPlayer(board[2][0].toString());
+        return;
+    }
 }
